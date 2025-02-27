a9c6b169062fb29d09d9e464246ccdd5633f1ec6
suriyaprakash-89 <tn.suriyaprakash29@gmail.com>
2025-02-27 17:34:39 +0530

    port change

--- pickupLine.js
+++ pickupLine.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { pickupLines } from "./api.js";
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
